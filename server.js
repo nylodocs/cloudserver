f0b2aea4828f3837b32af67527969a6dc6f40ef0
Dannylo Santos <nylopersan@gmail.com>
2025-03-06 09:58:50 +0100

    tests

--- server.js
+++ server.js
@@ -22,14 +22,16 @@ app.post("/executar", async (req, res) => {
   const maquinaLigada = await verificarMaquinaLigada();
 
   if (!maquinaLigada) {
+    return res.status(400).json({
+      error: "Máquina não está ligada ou servidor local não está rodando.",
+    });
+  } else {
     return res
-      .status(400)
-      .json({
-        error: "Máquina não está ligada ou servidor local não está rodando.",
-      });
+      .status(200)
+      .json({ message: "Máquina ligada e servidor local rodando." });
   }
 
-  const { script } = req.body;
+  //   const { script } = req.body;
 
   // try {
   //     const response = await axios.post(`${LOCAL_SERVER_URL}/executar-script`, { script });
@@ -37,12 +39,6 @@ app.post("/executar", async (req, res) => {
   // } catch (error) {
   //     res.status(500).json({ error: "Erro ao executar script." });
   // }
-  try {
-    const response = await axios.post(`${LOCAL_SERVER_URL}/status`, { script });
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao executar script." });
-  }
 });
 
 app.listen(PORT, () => {
